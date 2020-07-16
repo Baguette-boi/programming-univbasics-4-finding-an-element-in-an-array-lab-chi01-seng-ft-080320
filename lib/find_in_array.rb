12115d62dea28b82cb9f1349e036fe59e7668bd3
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-16 03:01:20 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,7 +5,7 @@ found_value_index = nil
 
 while count < array.length do
   if array[count]  == value_to_find
-  found_value_index = counter
+  found_value_index = count
   count +=1
 end
 found_value_index
