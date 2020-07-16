5dfbf6b3b35aedf9b8b89adcd6df30daa8832dee
Victor Manzanilla <vmanzanilla7@gmail.com>
2020-07-16 02:46:48 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,5 @@
 require 'pry'
 def find_element_index(array, value_to_find)
 binding.pry
-  counter = 0
-  
-  while array[counter] do
-    array[counter].include?(2)
-    r += 1counte
-  end
+puts 'jo'
 end
